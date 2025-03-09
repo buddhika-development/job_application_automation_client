18e8f089f4b1ebe4bd10bf6e567c667b035a5761
Buddhika Madusanka <buddhikadevelopment@gmail.com>
2025-03-10 01:43:02 +0530

    Get done file upload handling

--- src/Actions/HandleFormSubmit.js
+++ src/Actions/HandleFormSubmit.js
@@ -10,11 +10,12 @@ const HandleFormSubmit =(prevState,formData) => {
   const applicant_full_name = formData.get('applicant-name')
   const applicant_email_address = formData.get('applicant-mail')
   const applicant_contact_number = formData.get('applicant-contact')
+  const applicant_cv = formData.get('applicant-cv')
 
   let response;
 
   // validates the inputs
-  if (applicant_contact_number == '' || applicant_email_address == '' || applicant_full_name == ''){
+  if (applicant_contact_number == '' || applicant_email_address == '' || applicant_full_name == '' ||  applicant_cv.name == 'undefined'){
     console.log("please enter all the details")
 
     response = {
@@ -32,6 +33,11 @@ const HandleFormSubmit =(prevState,formData) => {
     else if(!phoneNumberValidation(applicant_contact_number)){
       return response = {
         error : 'Please enter valid phone number'
+      }
+    }
+    else if(applicant_cv.name == 'undefined') {
+      return response = {
+        error : "Please insert CV correctly.."
       }
     }
 
--- src/components/layout/ApplicantForm.jsx
+++ src/components/layout/ApplicantForm.jsx
@@ -7,6 +7,7 @@ import HandleFormSubmit from '@/Actions/HandleFormSubmit'
 import SubmitButton from '../ui/SubmitButton'
 import ErrorSection from '../ui/ErrorSection'
 import SuccessMessage from '../ui/SuccessMessage'
+import FileInput from '../ui/FileInput'
 
 const ApplicantForm = () => {
 
@@ -24,7 +25,7 @@ const ApplicantForm = () => {
         <InputSection type={'text'} placeholder={'John Wick'} name={'applicant-name'} lable={'Full Name'} />
         <InputSection type={'email'} placeholder={'example@sample.com'} name={'applicant-mail'} lable={'Email address'} />
         <InputSection type={'text'} placeholder={'phone number'} name={'applicant-contact'} lable={'Contact info'} />
-        <InputSection type={'file'} placeholder={''} name={'applicant-cv'} lable={'Resume'} />
+        <FileInput />
 
         {/* is there any error occur in the process */}
         {state?.error && (
